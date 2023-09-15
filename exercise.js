ad2a8ee33a5836384b2700d0de0a8ee693a3e55c
Chris1998c <contechristian.98@gmail.com>
2023-09-15 12:04:37 +0200

    ciao

---
+++ exercise.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+
+const jsonDeveloper = JSON.stringify(developer);
+
+console.log(jsonDeveloper);
